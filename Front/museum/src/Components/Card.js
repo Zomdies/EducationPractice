207f2caad7ff825f3be1b11431d172eb0d690756
HoneySpace <51043116+HoneySpace@users.noreply.github.com>
2020-07-12 21:57:36 +0900

    Card complete

--- Front/museum/src/Components/Card.js
+++ Front/museum/src/Components/Card.js
@@ -3,14 +3,23 @@ import '../CSS/AdminPanel.css'
 import '../CSS/Cards.css'
 
 const Card = (props) => {   
-
+    const {image} = props;
+    const {imageStyle} = props;
+    const {icons} = props;
+    const {iconsStyle} = props;
+    const {header} = props;
+    const {headerStyle} = props;
+    const {content} = props;
+    const {contentStyle} = props;
     
+    console.log(image)
+
     return(
             <div className="card layout">
-                <div style={{background:"crimson", gridArea:"image"}}></div>
-                <div style={{background:"darksalmon", gridArea:"header"}}></div>
-                <div style={{background:"indianred", gridArea:"content"}}></div>
-                <div style={{background:"bisque", gridArea:"icons"}}></div>
+                <div className="flex center" style={{gridArea:"image"}}><img className="min-image" style={{...imageStyle}} src={image}/></div>
+                <div className="header" style={{ ...headerStyle, gridArea:"header"}}>{header}</div>
+                <div className="content" style={{ ...contentStyle, gridArea:"content"}}>{content}</div>
+                <div className="icons"  style={{ justifyContent:"right" ,...iconsStyle, gridArea:"icons"}}>{icons}</div>
             </div>
         );
 };
--- Front/museum/src/Panels/ExpositionPanel.js
+++ Front/museum/src/Panels/ExpositionPanel.js
@@ -3,83 +3,32 @@ import '../CSS/AdminPanel.css'
 import '../CSS/Cards.css'
 
 import Card from '../Components/Card'
+import expositionImage from '../Image/Exposition.jpg'
 
 const ExpositionPanel = (props) => {   
-
-    useEffect(()=>{
-        const real = document.getElementById("real")
-        const fake = document.getElementById("fake")
-        const filler = document.getElementById("filler")
-
-        fake.style.top=real.style.top;
-        fake.style.left=real.style.left;
-
-
-        filler.style.height = real.scrollHeight+"px";
-    })
-
-    const scrollReal = (e)=>
-    {
-        e.preventDefault();
-        const real = document.getElementById("real")
-        const fake = document.getElementById("fake")
-        const filler = document.getElementById("filler")
-
-        filler.style.height = real.scrollHeight+"px";
-
-        const maxTopReal = real.scrollHeight-real.clientHeight;
-        const maxTopFake = fake.scrollHeight-fake.clientHeight;
-
-        fake.onscroll-=scrollFake;
-        fake.scrollTo(0,linmap(real.scrollTop,0,maxTopReal,0,maxTopFake));
-        fake.onscroll+=scrollFake;
-
-        console.log(`Реальный:${real.scrollTop} ложный:${fake.scrollTop}`)
-
-
-    }
-    const scrollFake = (e)=>
-    {
-        e.preventDefault();
-        const real = document.getElementById("real")
-        const fake = document.getElementById("fake")
-        const filler = document.getElementById("filler")
-
-        filler.style.height = real.scrollHeight+"px";
-
-        const maxTopReal = real.scrollHeight-real.clientHeight;
-        const maxTopFake = fake.scrollHeight-fake.clientHeight;
-
-        real.onscroll=null;
-        // real.scrollTo(0,linmap(real.scrollTop,0,maxTopFake,0,maxTopReal));
-        real.onscroll=scrollReal;
-
-        console.log(`Реальный:${real.scrollTop} ложный:${fake.scrollTop}`)
-
-    }
-    return(
-    
-    <div>
-        <div id="real" className="hidden-scroll container " onScroll={scrollReal} style={{zIndex:45}}>            
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>            
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-        </div>
-        <div id="fake" className="fake-container" onScroll={scrollFake} style={{zIndex:1, padding:0, display:"block"}}> 
-            <div id="filler"/>        
-        </div>
+  
+    return(    
+    <div className="flex center">
+        <div className="card-container" >            
+            <Card 
+                image={expositionImage}
+                header={<p>Chairs infinity and the picture of the world</p>}
+                icons={<><div style={{background:"#ff3", width:24, height:24}}/><div style={{background:"#a2b", width:24, height:24}}/><div style={{background:"#ff3", width:24, height:24}}/></>} 
+                content={"Волк слабее льва и тигра, но в цирке волк не выступает"}
+            />
+            <Card image={expositionImage}/>
+            <Card image={expositionImage}/>
+            <Card image={expositionImage}/>
+            <Card image={expositionImage}/>
+            <Card image={expositionImage}/>
+            <Card image={expositionImage}/>
+            <Card image={expositionImage}/>
+            <Card image={expositionImage}/>
+            <Card image={expositionImage}/>
+            <Card image={expositionImage}/>
+            <Card image={expositionImage}/>
+            <Card image={expositionImage}/>
+        </div>        
     </div>);
 };
 
