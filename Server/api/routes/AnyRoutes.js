1d69b1851b1b7b4a4f10167593b7a63fdf99f5f4
Zomdies <sablezubi@mail.ru>
2020-07-14 21:54:12 +0700

    7 commit Server
    Add new Route; /tokeLive
    Add new Image nophoto.png

--- Server/api/routes/AnyRoutes.js
+++ Server/api/routes/AnyRoutes.js
@@ -8,8 +8,21 @@ const dopFunction = require('../specialFunction');
 const User = require('../models/User');
 
 
-module.exports = () =>{
+module.exports = (access_token) =>{
 
+    route.post('/tokenLive', (req,res,next)=>{
+        let check = Object.values(access_token).indexOf(req.body.token,0);
+        console.log(check);
+        if (check !== 1){
+            res.status(500).json({
+                message : "Token is active",
+            })
+        }else{
+            res.status(404).json({
+                message : "Error token not found",
+            })
+        }
+    });
 
     return route;
 };
