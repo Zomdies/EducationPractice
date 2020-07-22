d4485db130b60829bd3ba7171187eb899d6be554
HoneySpace <51043116+HoneySpace@users.noreply.github.com>
2020-07-22 11:09:36 +0900

    minor back fix

--- Server/api/models/Exposition.js
+++ Server/api/models/Exposition.js
@@ -8,7 +8,15 @@ const expositionSchema = new mongooes.Schema({
     Date_Create: {type : Date, required: true}, //mongooes.Schema.Types.Date //Need convert string to Date
     Date_Open: {type : Date, required: true},
     Date_Close: {type : Date, required: true},
-    Image: {type : String, required: true}
+    Image: {type : String, required: true},
+    Hall:
+    {
+        ID_Hall: {
+            type: mongooes.Schema.Types.ObjectId,
+            default: null,
+            ref: "Hall"
+        }
+    }
 });
 
 module.exports = mongooes.model('Exposition', expositionSchema);
--- Server/api/models/Hall.js
+++ Server/api/models/Hall.js
@@ -3,7 +3,14 @@ const mongooes = require('mongoose');
 const hallSchema = new mongooes.Schema({
     // ID_Hall : mongooes.Schema.Types.ObjectId,
     Square: {type : Number, required: true},
-    Name: {type : String, required: true}
+    Name: {type : String, required: true},
+    Floor: {
+        ID_Floor:{
+            type: mongooes.Schema.Types.ObjectId,
+            default: null,
+            ref: "Floor"
+        }
+    }
 });
 
 module.exports = mongooes.model('Hall', hallSchema);
